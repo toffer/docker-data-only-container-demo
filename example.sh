d05c64156393428d76e0d14a2bdfb7df3c7de996
Tom Offermann <tom@offermann.us>
2013-12-04 21:19:25 -0800

    Add example.sh.
    * Shows how all the tiny Docker pieces work together.

---
+++ example.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Example script to show how all the pieces fit together.
+
+set -e
+
+echo "** Creating data container."
+docker run -name pandoc-data data true
+
+echo "** Cloning git repository to data container and saving doc directory."
+docker run -volumes-from pandoc-data git-clone https://github.com/isaacs/npm
+
+echo "** Converting doc directory to html using pandoc."
+docker run -volumes-from pandoc-data pandoc-convert /data/md /data/html
+
+echo "** Listing /data/html on data container to show resulting HTML files."
+docker run -volumes-from pandoc-data busybox ls -al /data/html
