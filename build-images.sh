826f4c4c14a3299943be5b320f3c5a17b0e2f1ce
Tom Offermann <tom@offermann.us>
2013-12-04 21:20:16 -0800

    Add build-images.sh.

---
+++ build-images.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "** Building pandoc"
+cd pandoc
+docker build -t pandoc .
+
+echo "** Building data"
+cd ../data
+docker build -t data .
+
+echo "** Building git-clone"
+cd ../git-clone
+docker build -t git-clone .
+
+echo "** Building pandoc-convert"
+cd ../pandoc-convert
+docker build -t pandoc-convert .
+
+
