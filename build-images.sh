7e3e2001bca26ef6bd3a36b339912c1e06e4aa5d
Tom Offermann <tom@offermann.us>
2013-12-05 09:54:05 -0800

    Simplify build-images.sh.

--- build-images.sh
+++ build-images.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-echo "** Building pandoc"
-cd pandoc
-docker build -t pandoc .
+NAMES='pandoc data git-clone pandoc-convert http-server'
 
-echo "** Building data"
-cd ../data
-docker build -t data .
-
-echo "** Building git-clone"
-cd ../git-clone
-docker build -t git-clone .
-
-echo "** Building pandoc-convert"
-cd ../pandoc-convert
-docker build -t pandoc-convert .
-
-echo "** Building http-server"
-cd ../http-server
-docker build -t http-server .
+for name in $NAMES; do
+    echo "** Building $name."
+    docker build -t $name $name
+    echo ""
+done
